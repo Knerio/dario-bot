d61bc34e77718d6e6079549f445c9ec68aaaf4c2
Dario <derioreal63@gmail.com>
2024-09-24 15:32:21 +0200

    fix invite url command

--- app/discord/commands/invite_url_command.rb
+++ app/discord/commands/invite_url_command.rb
@@ -1,3 +1,3 @@
-Bot.register_application_command(:invite_url, 'The invite url for the bot') do |event|
+Bot.command(:invite_url) do |event|
   event.respond content: Bot.invite_url, ephemeral: true
 end
